22d927e3986b310fec45b4a9d78f7c716a055657
s21290 <s21290@pjwstk.edu.pl>
2024-03-28 14:44:33 +0100

    Updated User classes

--- coinhub/src/main/java/pl/rafalprojects/coinhub/models/User.java
+++ coinhub/src/main/java/pl/rafalprojects/coinhub/models/User.java
@@ -2,8 +2,16 @@ package pl.rafalprojects.coinhub.models;
 
 import lombok.Getter;
 import lombok.Setter;
+import pl.rafalprojects.coinhub.models.enums.KindTransaction;
+import pl.rafalprojects.coinhub.models.enums.Role;
 
 @Getter
 @Setter
 public class User {
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Role role;
 }
---
+++ coinhub/src/main/java/pl/rafalprojects/coinhub/models/enums/Role.java
@@ -0,0 +1,6 @@
+package pl.rafalprojects.coinhub.models.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}
