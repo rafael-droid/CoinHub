7405cdcd693274e0d9873be4446b1a18be3cea56
s21290 <s21290@pjwstk.edu.pl>
2024-03-28 14:35:06 +0100

    Modified transaction controller

--- coinhub/src/main/java/pl/rafalprojects/coinhub/controllers/TransactionController.java
+++ coinhub/src/main/java/pl/rafalprojects/coinhub/controllers/TransactionController.java
@@ -1,7 +1,27 @@
 package pl.rafalprojects.coinhub.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import pl.rafalprojects.coinhub.models.Transaction;
+import pl.rafalprojects.coinhub.services.TransactionService;
 
 @RestController
+@RequestMapping("/coinhub")
 public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping("/transaction")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Iterable<Transaction> getAllTransaction(){
+        return transactionService.getTransactions();
+
+    }
+
 }
--- coinhub/src/main/java/pl/rafalprojects/coinhub/services/TransactionService.java
+++ coinhub/src/main/java/pl/rafalprojects/coinhub/services/TransactionService.java
@@ -1,8 +1,18 @@
 package pl.rafalprojects.coinhub.services;
 
 import org.springframework.stereotype.Service;
+import pl.rafalprojects.coinhub.models.Transaction;
+import pl.rafalprojects.coinhub.repositories.TransactionRepository;
 
 @Service
 public class TransactionService {
+    private final TransactionRepository transactionRepository;
 
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public Iterable<Transaction> getTransactions() {
+        return transactionRepository.findAll();
+    }
 }
